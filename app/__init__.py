96ffbdd186552ebea5d1e191874e47bd3510a5f6
Daniel Richmond <danielrichmond@Daniels-MacBook-Pro.local>
2023-04-20 22:00:48 -0500

    added file structure

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+
+
+
+from . import routes
+#from . import models
---
+++ app/routes.py
@@ -0,0 +1,9 @@
+from app import app, render_template
+
+
+@app.route('/')
+
+
+@app.route('/index')
+def index():
+    return render_template('index.html', title='Home')
