8406e21b8d1252f29654bf974eec69f47fad71b3
Daniel Richmond <danielrichmond@Daniels-MacBook-Pro.local>
2023-04-21 22:31:40 -0500

    added pages and styles

--- app/routes.py
+++ app/routes.py
@@ -7,4 +7,31 @@ from flask import render_template
 
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Home')
+    return render_template('index.html')
+
+@app.route('/signup')
+def sign_up():
+    """sign up page
+
+    Returns:
+        form: takes new user info
+    """
+    return render_template('signup.html')
+
+@app.route('/signin')
+def signin():
+    """allows the user to sign in
+
+    Returns:
+        form: signs the user in
+    """
+    return render_template('signin.html')
+
+@app.route('/cart')
+def cart():
+    """shopping cart page
+
+    Returns:
+        _type_: shows the user their current items they have placed in their cart
+    """
+    return render_template('cart.html')
--- config.py
+++ config.py
@@ -3,3 +3,6 @@ import os
 class Config():
     FLASK_APP = os.environ.get('FLASK_APP')
     FLASK_ENV = os.environ.get('FLASK_ENV')
+    #SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
